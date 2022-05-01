face0a3334e4168c4cd3bc33ff72131e2ffe9d34
Irfan Hasan <101458766+irfan7732@users.noreply.github.com>
2022-05-01 11:04:06 +0600

    Create run.py

---
+++ run.py
@@ -0,0 +1,17 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    os.system('rm -rf XNX1.2')
+    from free import ___RecodeSampah__
+    ___RecodeSampah__()
+elif bit == '32bit':
+    os.system('clear')
+    os.system('rm -rf XNX1.2')
+    print('Your Mobile Dosent Support This Tool')

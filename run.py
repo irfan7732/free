350e4e56b9b268fc58ab1a7fb7418e5a365694c1
Irfan Hasan <101458766+irfan7732@users.noreply.github.com>
2022-05-02 22:35:41 +0600

    Update run.py

--- run.py
+++ run.py
@@ -9,8 +9,7 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     os.system('clear')
     os.system('git pull')
-    print(' Facebook Is On Update All Method Are Going Lol')
-    print(' I will Try to Find New Method Inshaallah')
+    
 elif bit == '32bit':
     os.system('clear')
     os.system('git pull')

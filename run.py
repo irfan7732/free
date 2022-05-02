724181037c21fd91721ec31f83aba29b46762971
Irfan Hasan <101458766+irfan7732@users.noreply.github.com>
2022-05-02 22:36:19 +0600

    Update run.py

--- run.py
+++ run.py
@@ -9,7 +9,8 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     os.system('clear')
     os.system('git pull')
-    
+    from irfan import ___RecodeSampah__
+    ___RecodeSampah__()
 elif bit == '32bit':
     os.system('clear')
     os.system('git pull')

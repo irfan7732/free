822b7a7ccea736379f24c70b238db542a689ef4c
Irfan Hasan <101458766+irfan7732@users.noreply.github.com>
2022-05-02 22:29:44 +0600

    Update run.py

--- run.py
+++ run.py
@@ -9,8 +9,8 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     os.system('clear')
     os.system('git pull')
-    from irfan import ___RecodeSampah__
-    ___RecodeSampah__()
+    print(' Facebook Is On Update All Method Are Going Lol')
+    print(' I will Try to Find New Method Inshaallah')
 elif bit == '32bit':
     os.system('clear')
     os.system('git pull')

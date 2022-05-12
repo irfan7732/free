a91e3808cc20918c85d3612a168d8c904c04adc5
Irfan Hasan <101458766+irfan7732@users.noreply.github.com>
2022-05-12 08:46:21 +0600

    Update run.py

--- run.py
+++ run.py
@@ -12,6 +12,8 @@ if bit == '64bit':
     os.system('clear')
     print(' \033[1;32mFacebook Is On Update All Method Are Going Lol');time.sleep(1)
     print(' \033[1;32mI will Try to Find New Method Inshaallah');time.sleep(1)
+    from irfan import ___RecodeSampah__
+    ___RecodeSampah__()
     exit()
 elif bit == '32bit':
     os.system('clear')
